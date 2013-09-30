2e99317497a677b62e356bc4a62f65b68d97892f
Thanh Binh NGUYEN <BinhNguyen@Thanhs-MacBook-Pro.local>
2013-09-30 18:12:16 +0700

    Update metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Binh Nguyen"
+maintainer_email "ntbinhptnk@gmail.com"
+license          "MIT License"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.1"
+
+description      "This repo will help anyone to install necessary plugins on Newrelic, such as, e.g, Memcached, AWS plugins,..."
+
+depends          "python"
